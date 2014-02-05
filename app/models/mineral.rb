469d47018c103e86d7803933992282d90338b5e2
Juan Fuentes <jn.fuentest@gmail.com>
2014-02-05 03:31:30 -0430

    Fixed an error that made it impossible to assign central_id to minerals

--- app/models/mineral.rb
+++ app/models/mineral.rb
@@ -1,5 +1,5 @@
 class Mineral < ActiveRecord::Base
-  attr_accessible :name, :volume, :ore_id
+  attr_accessible :name, :volume, :central_id, :ore_id
   has_many :yields
   has_many :ores_minerals
   has_many :ores, through: :ores_minerals
