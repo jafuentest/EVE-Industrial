3952abced6a84f16b45d8dedcbc4aeb0c0353487
Juan Fuentes <jn.fuentest@gmail.com>
2014-02-05 18:43:55 -0430

    Greatly improved the response time of the mining spreadsheet
    This was achieved by replacing quicklook with marketstat from eve-central
    which allows to get price statistics for multiple items with a single request.

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -4,24 +4,31 @@ class SpreadsheetsController < ApplicationController
   def mining
     system = params.has_key?(:system) ? params[:system] : 30002187
     sale_tax = 1 - ((params.has_key?(:tax) ? params[:tax] : 0) / 100)
+    price_list = []
+    vars = Variation.pluck(:central_id).join(',')
+    
+    request = 'http://api.eve-central.com/api/marketstat?typeid=%s&usesystem=%s' % [vars, system]
+
+    xml = Curl.get(request).body_str
+    xml_doc  = Nokogiri::XML(xml)
+    percentiles = xml_doc.xpath("/evec_api/marketstat/type/buy/percentile")
+    
+    prices = { }
+    percentiles.each do |percentile|
+      prices [percentile.parent.parent['id'].to_i] = percentile.text.to_f
+    end
+    
     @ores = []
     Ore.all.each do |ore|
       ore_dict = { :name => ore.name}
       ore_dict[:variations] = []
       
-      ore.variations.each do |variation|
-        request = 'http://api.eve-central.com/api/quicklook?typeid=%s&usesystem=%s' % [variation.central_id, system]
-        xml = Curl.get(request).body_str
-        xml_doc  = Nokogiri::XML(xml)
-        price_list = xml_doc.xpath("/evec_api/quicklook/buy_orders/order/price")
-        prices = []
-        price_list.to_a.each do |price|
-          prices << price.text.to_f
-        end
-        max_price = prices.max
+      ore.variations.each do |var|
+        max_price = 5
     
-        variation = { :object => variation }
-        variation[:raw_revenue] = (1000/ore.volume)*max_price*sale_tax
+        variation = { :object => var }
+        variation[:price] = prices[var.central_id]
+        variation[:raw_revenue] = (1000/ore.volume)*prices[var.central_id]*sale_tax
         ore_dict[:variations] << variation
       end
       @ores << ore_dict
