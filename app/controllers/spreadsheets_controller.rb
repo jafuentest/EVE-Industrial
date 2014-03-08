37e2567a9e862276fc28a158088cc78347641927
Juan Fuentes <jn.fuentest@gmail.com>
2014-03-08 19:02:43 -0430

    Improved spreadsheets controller code.
    Apllied the methods used in the Planetary Interaction spreadsheet to the other
    spreadsheets to reduce coding and also improve readability.

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -4,7 +4,6 @@ class SpreadsheetsController < ApplicationController
   def planetary_interaction
     if (params.has_key? :region) && !params[:region].empty?
       items = PlanetaryCommodity.pluck :central_id
-      save_location_values
       
       # Get data from EVE-Central
       if params[:system].empty?
@@ -23,8 +22,7 @@ class SpreadsheetsController < ApplicationController
       processors[4] = params[:high_tech_processors].to_i
       
       # Save in cookies
-      cookies[:region] = params[:region]
-      cookies[:system] = params[:system]
+      save_location_values
       cookies[:basic_processors] = params[:basic_processors]
       cookies[:advanced_processors] = params[:advanced_processors]
       cookies[:high_tech_processors] = params[:high_tech_processors]
@@ -35,7 +33,7 @@ class SpreadsheetsController < ApplicationController
       PlanetaryCommodity.where(:tier => 1).each do |pc|
         material = { :name => pc.name, :id => pc.id }
         material[:revenue] = pc.hour_revenue prices[:buy][pc.central_id], processors
-        material[:inversion_return] = pc.processing_revenue prices[:buy], prices[:sell], customs_office_tax, market_tax
+        material[:inversion_return] = pc.processing_revenue prices, customs_office_tax, market_tax
         resource = pc.schematics[0].input
         material[:resource] = { :name => resource.name, :id => resource.id, :price => prices[:buy][resource.central_id] }
         @materials << material
@@ -44,7 +42,7 @@ class SpreadsheetsController < ApplicationController
       PlanetaryCommodity.where(:tier => 2).each do |pc|
         resource = { :name => pc.name, :id => pc.id }
         resource[:revenue] = pc.hour_revenue prices[:buy][pc.central_id], processors
-        resource[:inversion_return] = pc.processing_revenue prices[:buy], prices[:sell], customs_office_tax, market_tax
+        resource[:inversion_return] = pc.processing_revenue prices, customs_office_tax, market_tax
         @commodities << resource
       end
     end
@@ -59,9 +57,7 @@ class SpreadsheetsController < ApplicationController
   
   def refining
     if (params.has_key? :region) && !params[:region].empty?
-      ore_ids = Variation.pluck(:central_id)
-      mineral_ids = Mineral.pluck(:central_id)
-      items = ore_ids.clone.concat(mineral_ids).join(',')
+      items = Variation.pluck(:central_id).concat(Mineral.pluck :central_id)
       
       # Retrieve user specific data
       station_yield = params[:station_yield]
@@ -77,49 +73,24 @@ class SpreadsheetsController < ApplicationController
       end
       
       # Save in cookies
-      cookies[:region] = params[:region]
-      cookies[:system] = params[:system]
+      save_location_values
       cookies[:refinery_tax] = params[:refinery_tax]
       cookies[:market_tax] = params[:market_tax]
       cookies[:station_yield] = params[:station_yield]
       cookies[:refining_skill] = params[:refining_skill]
       cookies[:refinery_efficiency_skill] = params[:refinery_efficiency_skill]
       
-      # Request information from eve-central and parse it to an array
+      # Get data from EVE-Central
       if params[:system].empty?
-        region = Region.find params[:region]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&regionlimit=%s' % [items, region.central_id]
-        @params = { :region => region.id }
-        @location = '%s Region' % region.name
+        prices = request_prices items, params[:region], :region
       else
-        region = Region.find params[:region]
-        system = System.find params[:system]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&usesystem=%s' % [items, system.central_id]
-        @params = { :region => region.id, :system => system.id }
-        @location = '%s System' % system.name
-      end
-      xml = Curl.get(request).body_str
-      xml_doc = Nokogiri::XML(xml)
-      buy_percentiles = xml_doc.xpath('/evec_api/marketstat/type/buy/percentile')
-      sell_percentiles = xml_doc.xpath('/evec_api/marketstat/type/sell/percentile')
-      
-      # Dump the prices array into the hashes used for revenue calculation
-      ore_prices = { }
-      sell_percentiles.each do |percentile|
-        item_id = percentile.parent.parent['id'].to_i
-        ore_prices [item_id] = percentile.text.to_f
-      end
-      
-      mineral_prices = { }
-      buy_percentiles.each do |percentile|
-        item_id = percentile.parent.parent['id'].to_i
-        mineral_prices [item_id] = percentile.text.to_f
+        prices = request_prices items, params[:system], :system
       end
       
       @minerals = []
       Mineral.all.each do |min|
         mineral = { :id => min.id, :name => min.name }
-        mineral[:price] = mineral_prices[min.central_id]
+        mineral[:price] = prices[:buy][min.central_id]
         @minerals << mineral
       end
       
@@ -127,13 +98,13 @@ class SpreadsheetsController < ApplicationController
       @variations = []
       Variation.all.each do |var|
         variation = { :id => var.id, :name => var.name }
-        variation[:price] = ore_prices[var.central_id]
-        refining_results = var.refine_revenue(mineral_prices, station_yield, skills, refinery_tax)
+        variation[:price] = prices[:sell][var.central_id]
+        refining_results = var.refine_revenue prices, station_yield, skills, refinery_tax
         variation[:efficiency] = refining_results[:efficiency]
         if variation[:price] == 0
           variation[:refine_revenue] = -100 # lower than any possible value
         else
-          cost = var.raw_revenue(ore_prices[var.central_id])
+          cost = var.raw_revenue prices[:sell][var.central_id]
           profit = refining_results[:revenue] * (1 - market_tax.to_f/100)
           profit -= cost
           profit /= cost
@@ -141,16 +112,11 @@ class SpreadsheetsController < ApplicationController
         end
         @variations << variation
       end
-      
-      @variations.sort_by! { |v| v[:refine_revenue] }.reverse!
     end
     
     # Set initial values for input fields
+    set_location_values
     @ores = Ore.all
-    @regions = Region.all
-    @region = cookies[:region]
-    @systems = @region.nil? ? nil : Region.find(@region).systems
-    @system = cookies[:system]
     @station_yield = cookies[:station_yield]
     @refining_skill = cookies[:refining_skill]
     @refinery_efficiency_skill = cookies[:refinery_efficiency_skill]
@@ -165,9 +131,7 @@ class SpreadsheetsController < ApplicationController
   
   def ore_mining
     if (params.has_key? :region) && !params[:region].empty?
-      ore_ids = Variation.pluck(:central_id)
-      mineral_ids = Mineral.pluck(:central_id)
-      items = ore_ids.clone.concat(mineral_ids).join(',')
+      items = Variation.pluck(:central_id).concat(Mineral.pluck :central_id)
       
       # Retrieve user specific data
       station_yield = params[:station_yield]
@@ -182,49 +146,26 @@ class SpreadsheetsController < ApplicationController
       end
       
       # Save in cookies
-      cookies[:region] = params[:region]
-      cookies[:system] = params[:system]
+      save_location_values
       cookies[:refinery_tax] = params[:refinery_tax]
       cookies[:station_yield] = params[:station_yield]
       cookies[:refining_skill] = params[:refining_skill]
       cookies[:refinery_efficiency_skill] = params[:refinery_efficiency_skill]
       
-      # Request information from eve-central and parse it to an array
+      # Get data from EVE-Central
       if params[:system].empty?
-        region = Region.find params[:region]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&regionlimit=%s' % [items, region.central_id]
-        @params = { :region => region.id }
-        @location = '%s Region' % region.name
+        prices = request_prices items, params[:region], :region
       else
-        region = Region.find params[:region]
-        system = System.find params[:system]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&usesystem=%s' % [items, system.central_id]
-        @params = { :region => region.id, :system => system.id }
-        @location = '%s System' % system.name
-      end
-      xml = Curl.get(request).body_str
-      xml_doc = Nokogiri::XML(xml)
-      percentiles = xml_doc.xpath('/evec_api/marketstat/type/buy/percentile')
-      
-      # Dump the prices array into the hashes used for revenue calculation
-      ore_prices = { }
-      mineral_prices = { }
-      percentiles.each do |percentile|
-        item_id = percentile.parent.parent['id'].to_i
-        if ore_ids.include? item_id
-          ore_prices [item_id] = percentile.text.to_f
-        else
-          mineral_prices [item_id] = percentile.text.to_f
-        end
+        prices = request_prices items, params[:system], :system
       end
       
       # Retrieve the information to show about each ore variation
       @variations = []
       Variation.all.each do |var|
         variation = { :id => var.id, :name => var.name }
-        variation[:price] = ore_prices[var.central_id]
-        variation[:raw_revenue] = var.raw_revenue(ore_prices[var.central_id])
-        refining_results = var.refine_revenue(mineral_prices, station_yield, skills, refinery_tax)
+        variation[:price] = prices[:buy][var.central_id]
+        variation[:raw_revenue] = var.raw_revenue(prices[:buy][var.central_id])
+        refining_results = var.refine_revenue(prices[:buy], station_yield, skills, refinery_tax)
         variation[:refine_revenue] = refining_results[:revenue]
         variation[:refine_yield] = refining_results[:yield]
         variation[:volume] = refining_results[:volume]
@@ -238,11 +179,8 @@ class SpreadsheetsController < ApplicationController
     end
     
     # Set initial values for input fields
+    set_location_values
     @ores = Ore.all
-    @regions = Region.all
-    @region = cookies[:region]
-    @systems = @region.nil? ? nil : Region.find(@region).systems
-    @system = cookies[:system]
     @station_yield = cookies[:station_yield]
     @refining_skill = cookies[:refining_skill]
     @refinery_efficiency_skill = cookies[:refinery_efficiency_skill]
@@ -256,9 +194,7 @@ class SpreadsheetsController < ApplicationController
   
   def ice_mining
     if (params.has_key? :region) && !params[:region].empty?
-      ore_ids = IceOre.pluck(:central_id)
-      products_ids = IceProduct.pluck(:central_id)
-      items = ore_ids.clone.concat(products_ids).join(',')
+      items = IceProduct.pluck(:central_id).concat(IceOre.pluck :central_id)
       
       # Retrieve user specific data
       station_yield = params[:station_yield]
@@ -269,49 +205,26 @@ class SpreadsheetsController < ApplicationController
       skills[:ice_processing_skill] = params[:ice_processing_skill]
       
       # Save in cookies
-      cookies[:region] = params[:region]
-      cookies[:system] = params[:system]
+      save_location_values
       cookies[:refinery_tax] = params[:refinery_tax]
       cookies[:station_yield] = params[:station_yield]
       cookies[:refining_skill] = params[:refining_skill]
       cookies[:refinery_efficiency_skill] = params[:refinery_efficiency_skill]
       cookies[:ice_processing_skill] = params[:ice_processing_skill]
       
-      # Request information from eve-central and parse it to an array
+      # Get data from EVE-Central
       if params[:system].empty?
-        region = Region.find params[:region]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&regionlimit=%s' % [items, region.central_id]
-        @params = { :region => region.id }
-        @location = '%s Region' % region.name
+        prices = request_prices items, params[:region], :region
       else
-        region = Region.find params[:region]
-        system = System.find params[:system]
-        request = 'http://api.eve-central.com/api/marketstat?typeid=%s&usesystem=%s' % [items, system.central_id]
-        @params = { :region => region.id, :system => system.id }
-        @location = '%s System' % system.name
-      end
-      xml = Curl.get(request).body_str
-      xml_doc = Nokogiri::XML(xml)
-      percentiles = xml_doc.xpath('/evec_api/marketstat/type/buy/percentile')
-      
-      # Dump the prices array into the hashes used for revenue calculation
-      ore_prices = { }
-      product_prices = { }
-      percentiles.each do |percentile|
-        item_id = percentile.parent.parent['id'].to_i
-        if ore_ids.include? item_id
-          ore_prices [item_id] = percentile.text.to_f
-        else
-          product_prices [item_id] = percentile.text.to_f
-        end
+        prices = request_prices items, params[:system], :system
       end
       
       # Retrieve the information to show about each ore variation
       @ores = []
       IceOre.all.each do |ore|
         ore_hash = { :id => ore.id, :name => ore.name }
-        ore_hash[:price] = ore_prices[ore.central_id]
-        refining_results = ore.refine_revenue(product_prices, station_yield, skills, refinery_tax)
+        ore_hash[:price] = prices[:buy][ore.central_id]
+        refining_results = ore.refine_revenue(prices[:buy], station_yield, skills, refinery_tax)
         ore_hash[:refine_revenue] = refining_results[:revenue]
         ore_hash[:refine_yield] = refining_results[:yield]
         ore_hash[:volume] = refining_results[:volume]
@@ -325,13 +238,11 @@ class SpreadsheetsController < ApplicationController
     end
     
     # Set initial values for input fields
-    @regions = Region.all
-    @region = cookies[:region]
-    @systems = @region.nil? ? nil : Region.find(@region).systems
-    @system = cookies[:system]
+    set_location_values
     @station_yield = cookies[:station_yield]
     @refining_skill = cookies[:refining_skill]
     @refinery_efficiency_skill = cookies[:refinery_efficiency_skill]
+    @ice_processing_skill = cookies[:refinery_efficiency_skill]
     @refinery_tax = cookies[:refinery_tax]
   end
   
@@ -372,12 +283,19 @@ class SpreadsheetsController < ApplicationController
     { :buy => buy_prices, :sell => sell_prices }
   end
   
-  # Sets all values, even the ones not used by all spreadsheets
-  def set_input_values
+  # Sets previously used location as default
+  def set_location_values
     @regions = Region.all
     @region = cookies[:region]
     @systems = @region.nil? ? nil : Region.find(@region).systems
     @system = cookies[:system]
+    nil
+  end
+  
+  # Saves location requested to cookies
+  def save_location_values
+    cookies[:region] = params[:region]
+    cookies[:system] = params[:system]
     nil
   end
 end
