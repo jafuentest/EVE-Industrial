c539e1ea197e98fa20736feebf6a5003598b2a93
Juan Fuentes <jn.fuentest@gmail.com>
2014-03-12 00:39:45 -0430

    Hid all links to edit actions from the view for normal users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :is_admin
+  before_filter :can_edit
+  
+  def can_edit
+    @can_edit = session[:is_admin]
+  end
   
   def is_admin
     if session[:user_id].nil? || ! session[:is_admin]
