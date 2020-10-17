115caea509f443c586e2dfde1875b83d7d86986f
Juan Fuentes <jn.fuentest@gmail.com>
2020-10-17 01:21:00 -0400

    Add simple page to test ESI API calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   devise_scope :user do
-    get 'logout', to: 'users/sessions#destroy', as: :logout
-    get 'login', to: 'users/sessions#create', as: :login
+    delete 'logout', to: 'users/sessions#destroy', as: :logout
+    post 'login', to: 'users/sessions#create', as: :login
+    get 'login', to: 'users/sessions#new'
   end
 
   resources :planetary_commodities, only: %i[index show] do
     post :update_prices, on: :collection
   end
+
+  scope '', controller: :pages do
+    get :character_data
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
