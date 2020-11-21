ba3c2e5a907436084b43cd5e9f7f34b3c026fdca
Juan Fuentes <jn.fuentest@gmail.com>
2020-11-21 04:42:39 -0400

    Import jQuery to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'jquery'
+import $ from 'jquery';
+
 import 'popper.js'
 import 'bootstrap'
 import 'bootstrap-table'
