bdcaa2eb15b597a740bef017f46943ddd4225e6d
Juan Fuentes <jn.fuentest@gmail.com>
2020-12-11 01:34:37 -0400

    Obtain all character orders

--- app/models/esi.rb
+++ app/models/esi.rb
@@ -17,8 +17,7 @@ class ESI
 
   def self.verify_access_token(access_token)
     uri = URI("#{AUTH_BASE_URL}/oauth/verify")
-    req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "Bearer #{access_token}"
+    req = request_from_uri(uri, access_token)
 
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(req)
@@ -42,9 +41,7 @@ class ESI
 
   def self.fetch_character_planets(user)
     uri = URI("#{ESI_BASE_URL}/characters/#{user.character_id}/planets/")
-    req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "Bearer #{user.auth_token}"
-    req['Accept'] = 'application/json'
+    req = request_from_uri(uri, user.auth_token)
 
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(req)
@@ -59,16 +56,33 @@ class ESI
     end
   end
 
-  private_class_method def self.fetch_planet_details(user, planet_id)
-    uri = URI("#{ESI_BASE_URL}/characters/#{user.character_id}/planets/#{planet_id}")
-    req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "Bearer #{user.auth_token}"
+  def self.fetch_character_market_orders(user)
+    uri = URI("#{ESI_BASE_URL}/characters/#{user.character_id}/orders/")
+    req = request_from_uri(uri, user.auth_token)
 
     res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(req)
     end
 
     JSON.parse(res.body)
+  end
+
+  private_class_method def self.fetch_planet_details(user, planet_id)
+    uri = URI("#{ESI_BASE_URL}/characters/#{user.character_id}/planets/#{planet_id}")
+    req = request_from_uri(uri, user)
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+
+    JSON.parse(res.body)
+  end
+
+  private_class_method def self.request_from_uri(uri, access_token = nil)
+    req = Net::HTTP::Get.new(uri)
+    req['Authorization'] = "Bearer #{access_token}" if access_token.present?
+    req['Accept'] = 'application/json'
+    req
   end
 
   private_class_method def self.fetch_token_request(code)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  self.primary_key = :id
+
+  has_many :orders
 end
