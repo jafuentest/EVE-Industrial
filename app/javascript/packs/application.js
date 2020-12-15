ebf7a5e17b35a47827dfa59c823dfa5b3bc297bb
Juan Fuentes <jn.fuentest@gmail.com>
2020-12-14 20:49:45 -0400

    Set timeout for esi universe id/name replacing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,18 +19,20 @@ require("turbolinks").start()
 // const imagePath = (name) => images(name, true)
 
 document.addEventListener("turbolinks:load", () => {
-  $("[data-esi-id]").each((i, e) => {
-    const id = e.attributes['data-esi-id'].value
-    const type = e.attributes['data-esi-type'].value
-    let url = 'https://esi.evetech.net/latest/universe/'
+  setTimeout(() => {
+    $("[data-esi-id]").each((i, e) => {
+      const id = e.attributes['data-esi-id'].value
+      const type = e.attributes['data-esi-type'].value
+      let url = 'https://esi.evetech.net/latest/universe/'
 
-    if (type === "item")
-      url += "types/" + id
-    else if (type === "planet")
-      url += "planets/" + id
-    else if (type === "system")
-      url += "systems/" + id
+      if (type === "item")
+        url += "types/" + id
+      else if (type === "planet")
+        url += "planets/" + id
+      else if (type === "system")
+        url += "systems/" + id
 
-    $.ajax(url).done(data => { e.innerHTML = data.name })
-  })
+      $.ajax(url).done(data => { e.innerHTML = data.name })
+    })
+  }, 1000);
 })
