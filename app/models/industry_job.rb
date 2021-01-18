9a99364adc0ad8519aa93844605fd94b957481b7
Juan Fuentes <jn.fuentest@gmail.com>
2021-01-18 00:58:59 -0400

    Fetch character's jobs

--- app/models/industry_job.rb
+++ app/models/industry_job.rb
@@ -1,3 +1,18 @@
 class IndustryJob < ApplicationRecord
+  ESI_ATTRIBUTES = %w[
+    character_id blueprint_id blueprint_type_id product_type_id activity_id station_id installer_id
+    start_date end_date runs licensed_runs probability status
+  ].freeze
+
   self.primary_key = :id
+
+  def self.update_character_industry_jobs(character)
+    ESI.fetch_character_industry_jobs(character).each do |esi_job|
+      job = find_or_initialize_by(id: esi_job['job_id'])
+      next if job.persisted?
+
+      job.assign_attributes(esi_job.slice(*ESI_ATTRIBUTES))
+      job.save!
+    end
+  end
 end
