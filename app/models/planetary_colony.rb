8a1112e133761802166b3237d1fcc08181ce26a1
Juan Fuentes <jn.fuentest@gmail.com>
2021-04-15 00:48:26 -0400

    Handle persistence of hash fields as json strings

--- app/models/planetary_colony.rb
+++ app/models/planetary_colony.rb
@@ -1,3 +1,19 @@
 class PlanetaryColony < ApplicationRecord
   belongs_to :character
+
+  def extractors
+    JSON.parse(self[:extractors]).with_indifferent_access
+  end
+
+  def extractors=(o)
+    self[:extractors] = o.to_json
+  end
+
+  def factories
+    JSON.parse(self[:factories]).with_indifferent_access
+  end
+
+  def factories=(o)
+    self[:factories] = o.to_json
+  end
 end
