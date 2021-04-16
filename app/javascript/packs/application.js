1f6ae29d89301a57b46e5bec52657123d49c769a
Juan Fuentes <jn.fuentest@gmail.com>
2021-04-15 21:18:38 -0400

    Fix popperjs install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'popper.js'
 import 'bootstrap'
 import 'bootstrap-table'
 import 'stylesheets/application'
 
 require('@rails/ujs').start()
 require('turbolinks').start()
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,7 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
+    jQuery: 'jquery/src/jquery'
   })
 )
 
