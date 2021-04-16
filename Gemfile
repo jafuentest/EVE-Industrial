cee49fa1db7db6505c209673d4c9140af8223cc0
Juan A. Fuentest T <jn.fuentest@gmail.com>
2021-04-15 21:47:08 -0400

    Feature/upgrade webacker (#18)
    * Updgrade webpacker
    
    * Fix boostratp integration
    
    * Fix popperjs install
    
    * Fix bootstrap-table installation

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
-gem 'webpacker', '~> 4.0'
+gem 'webpacker', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -24,9 +24,6 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise', '~> 4.7', '>= 4.7.3'
-
-gem 'bootstrap', '~> 5.0.0.alpha1'
-gem 'bootstrap-table-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'popper.js'
 import 'bootstrap'
 import 'bootstrap-table'
 
-require("@rails/ujs").start()
-require("turbolinks").start()
+import 'stylesheets/application'
+import 'bootstrap-table/dist/bootstrap-table.min.css'
 
+require('@rails/ujs').start()
+require('turbolinks').start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,18 +19,18 @@ require("turbolinks").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-document.addEventListener("turbolinks:load", () => {
-  $("[data-esi-id]").each((i, e) => {
+document.addEventListener('turbolinks:load', () => {
+  $('[data-esi-id]').each((i, e) => {
     const id = e.attributes['data-esi-id'].value
     const type = e.attributes['data-esi-type'].value
     let url = 'https://esi.evetech.net/latest/universe/'
 
-    if (type === "item")
-      url += "types/" + id
-    else if (type === "planet")
-      url += "planets/" + id
-    else if (type === "system")
-      url += "systems/" + id
+    if (type === 'item')
+      url += 'types/' + id
+    else if (type === 'planet')
+      url += 'planets/' + id
+    else if (type === 'system')
+      url += 'systems/' + id
 
     $.ajax(url).done(data => { e.innerHTML = data.name })
   })
