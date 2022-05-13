5d12f2f5370cf96214391cf6cd741bd48e10bdf7
Juan Fuentes <jn.fuentest@gmail.com>
2022-05-13 19:23:05 -0400

    Update config files

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ require 'action_mailer/railtie'
 # require 'action_mailbox/engine'
 # require 'action_text/engine'
 require 'action_view/railtie'
-# require 'action_cable/engine'
-require 'sprockets/railtie'
+require 'action_cable/engine'
 # require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,7 +22,7 @@ module EVEIndustrial; end
 
 class EVEIndustrial::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 6.0
+  config.load_defaults 7.0
 
   # Configuration for the application, engines, and railties goes here.
   #
