e3e94a8de85387e64f8a24f7b779e93c73b802fc
Juan Fuentes <jn.fuentest@gmail.com>
2022-05-13 21:44:19 -0400

    Replace webpacker with js-bundling and esbuild

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
+
+import * as bootstrap from 'bootstrap'
+import * as bootstrapTable from 'bootstrap-table'
+import './table-sorting'
