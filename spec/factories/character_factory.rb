6ee135f890daa8056a5a46e6b64220f4f9146b6d
Juan Fuentes <juanf@scribd.com>
2023-05-06 01:57:52 +0200

    Complete character specs coverage

--- spec/factories/character_factory.rb
+++ spec/factories/character_factory.rb
@@ -13,8 +13,22 @@ FactoryBot.define do
   #  updated_at         :datetime         not null
   #
 
+  sequence(:character_id)
+
   factory :character do
     association :user
+
+    character_id
+    esi_refresh_token { "esi_refresh_token#{character_id}" }
+    esi_auth_token { "esi_auth_token#{character_id}" }
+    esi_expires_on { DateTime.now + 10.minutes }
+
+    character_name { "Factory Character#{character_id}" }
+    character_portrait { "https://images.evetech.net/characters/#{character_id}/portrait?tenant=tranquility&size=64" }
+
+    scopes { 'esi-markets.read_character_orders.v1 esi-markets.read_corporation_orders.v1' }
+    token_type { 'Character' }
+    owner_hash { "owner_hash#{character_id}" }
 
     trait :without_verification_data do
       character_name { nil }
