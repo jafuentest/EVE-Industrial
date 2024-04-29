7d6afa69e70521a49c5b934be4294dc441b70f73
Juan Fuentes <jn.fuentest@gmail.com>
2024-04-29 15:42:44 +0200

    Item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,41 +5,53 @@ RSpec.describe Item, type: :model do
     expect(FactoryBot.build(:item)).to be_valid
   end
 
-  describe '#set_name' do
-    it "calls the ESI to set the item's name" do
-      star_name = SecureRandom.alphanumeric
-      allow(ESI).to receive(:fetch_item_name).and_return(star_name)
-      star = FactoryBot.build(:item, name: nil)
-      star.set_name
+  describe 'associations' do
+    skip { is_expected.to have_many(:industry_jobs) }
+    it { is_expected.to have_many(:orders) }
+  end
 
-      expect(star.name).to eq(star_name)
+  describe '#save' do
+    let(:item) { FactoryBot.build(:item, name: nil) }
+    let(:item_name) { "Item Name #{rand(10)}" }
+
+    before(:each) do
+      allow(ESI).to receive(:fetch_item_name).and_return(item_name)
+    end
+
+    subject { item.save }
+
+    it "Sets the item's name" do
+      subject
+      expect(item.name).to eq(item_name)
+    end
+
+    it "Call's ESI to fetch the item's name" do
+      expect(ESI).to receive(:fetch_item_name).once
+      subject
     end
   end
 
   describe '#create_items' do
-    let(:test_items) { 2 }
+    let(:item_ids) { (1..2).to_a }
+
     before(:each) do
       expect(ESI).to receive(:fetch_item_name).twice
         .and_return(SecureRandom.alphanumeric)
     end
 
-    it 'creates an item object for each id given' do
-      item_ids = (1..test_items).to_a
-      Item.destroy_all
-      Item.create_items(item_ids)
+    subject { Item.create_items(item_ids) }
 
-      expect(Item.count).to eq(item_ids.size)
+    it 'creates an item object for each id given' do
+      subject
+
       expect(Item.pluck(:id).sort).to eq(item_ids)
     end
 
-    it 'does not try to create duplicates' do
-      item_ids = (1..test_items).to_a
-      FactoryBot.create(:item, id: 1)
-      FactoryBot.create(:item, id: 2)
-      Item.create_items(item_ids)
+    it 'does not create duplicates' do
+      item_ids.each { |id| FactoryBot.create(:item, id:) }
+      subject
 
       expect(Item.count).to eq(item_ids.size)
-      expect(Item.pluck(:id).sort).to eq(item_ids)
     end
   end
 end
