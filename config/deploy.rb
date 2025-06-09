860550a5f1eb575d2070ef3983b71a1f4f3e615f
Juan Fuentes <jn.fuentest@gmail.com>
2025-06-09 15:19:11 +0200

    Move cap production settings to production file

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,8 +39,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :rails_env, 'production'
-
 set :bundle_without, %w[development test]
 
 set :ssh_options, { forward_agent: true, keys: %w[~/.ssh/id_rsa.pub] }
