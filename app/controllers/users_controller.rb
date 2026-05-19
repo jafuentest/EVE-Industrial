4dd7d7d07f203bcc2d3f30fcfe6157420a55673e
Juan Fuentes <jn.fuentest@gmail.com>
2026-05-19 23:18:35 +0200

    Fix rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def settings; end
 
   def remove_character
-    Character.find(params[:id]).destroy
+    Character.find(params.expect(:id)).destroy
     redirect_to settings_path
   end
 end
