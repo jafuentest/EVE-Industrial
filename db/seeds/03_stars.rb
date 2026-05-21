e488ccd0428051c1d9797f76a7b3ff6cf4090614
Juan Fuentes <jn.fuentest@gmail.com>
2026-05-21 04:54:32 +0200

    Create separate seed files for each model

---
+++ db/seeds/03_stars.rb
@@ -0,0 +1,4 @@
+Star.find_or_create_by!(id: 30_000_142, name: 'Jita', constellation_id: 20_000_020, region_id: 10_000_002)
+Star.find_or_create_by!(id: 30_002_187, name: 'Amarr', constellation_id: 20_000_322, region_id: 10_000_043)
+Star.find_or_create_by!(id: 30_002_659, name: 'Dodixie', constellation_id: 20_000_389, region_id: 10_000_032)
+Star.find_or_create_by!(id: 30_002_874, name: 'R1O-GN', constellation_id: 20_000_422, region_id: 10_000_034)
