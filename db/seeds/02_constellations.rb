f97ba06fc709f0250ef18ce7826ab69fa9419fb1
Juan Fuentes <jn.fuentest@gmail.com>
2026-05-21 04:54:32 +0200

    Create separate seed files for each model

---
+++ db/seeds/02_constellations.rb
@@ -0,0 +1,4 @@
+Constellation.find_or_create_by!(id: 20_000_020, region_id: 10_000_002, name: 'Kimotoro')
+Constellation.find_or_create_by!(id: 20_000_322, region_id: 10_000_043, name: 'Throne Worlds')
+Constellation.find_or_create_by!(id: 20_000_389, region_id: 10_000_032, name: 'Coriault')
+Constellation.find_or_create_by!(id: 20_000_422, region_id: 10_000_034, name: 'P-B2NE')
