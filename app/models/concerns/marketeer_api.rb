6d4f11b471b2c403546157a1f03e81335d096d99
Juan Fuentes <jn.fuentest@gmail.com>
2026-05-21 04:36:43 +0200

    Replace API with EVE Tycoon

--- app/models/concerns/marketeer_api.rb
+++ app/models/concerns/marketeer_api.rb
@@ -1,17 +1,38 @@
 module MarketeerAPI
+  require 'net/http'
+
   extend ActiveSupport::Concern
 
-  BASE_URL = 'https://api.evemarketer.com/ec/marketstat/json'.freeze
+  BASE_URL = 'https://evetycoon.com/api/v1'.freeze
 
   class_methods do
-    def fetch_prices_for(items:, star_id:)
-      raise "Eve Marketeer API no longer exists, request: #{request_uri(items, star_id)}"
+    def fetch_prices_for(items:, region_id: default_region)
+      items.map do |item_id|
+        uri = request_uri(item_id, region_id)
+        response = Net::HTTP.get_response(uri)
+
+        # TODO: Handle non-successful responses
+        JSON.parse(response.body)
+          .merge(expires_at_from_response(response))
+          .merge('item_id' => item_id)
+      end
     end
 
     private
 
-    def request_uri(items, star_id)
-      url = "#{BASE_URL}?typeid=#{items.join(',')}&usesystem=#{star_id}"
+    def expires_at_from_response(response)
+      header_value = response.each_header.find { |e| e.first == 'expires' }.last
+      return {} unless header_value
+
+      { "expires_at" => Time.httpdate(header_value) }
+    end
+
+    def default_region
+      Region::IDs::THE_FORGE
+    end
+
+    def request_uri(item_id, region_id)
+      url = "#{BASE_URL}/market/stats/#{region_id}/#{item_id}"
       URI(url)
     end
   end
